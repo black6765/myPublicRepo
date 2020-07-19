eb95a0833d2930ea78e7a5ae89fd279d6ddd5d77
black6765 <black6765@gmail.com>
2020-07-19 23:38:39 +0900

    modify almost code

--- add-nbo.c
+++ add-nbo.c
@@ -1,44 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdint.h>
-#include "add-nbo.h"
+#include <netinet/in.h>
 
 int main(int argc, char* argv[])
 
 {
-	// argv[1]을 위한 배열입니다. 
-	char str1[SIZE];
-	// argv[2]를 위한 배열입니다. 
-	char str2[SIZE];
+	uint32_t buffer1;
+	uint32_t buffer2;
 	
 	FILE *fp1 = NULL;
 	FILE *fp2 = NULL;
 	
-	// echo -n -e 명령어로 생성된 바이너리 파일을 엽니다. 
+	// open bin file 
 	fp1 = fopen(argv[1], "rb");
 	fp2 = fopen(argv[2], "rb");
 	
-	// 바이너리 파일을 읽어 str1, str2에 각각 저장합니다. 
-	fread(str1, sizeof(char), SIZE, fp1);
-	fread(str2, sizeof(char), SIZE, fp2);
+	// read bin file
+	fread(&buffer1, sizeof(char), sizeof(uint32_t), fp1);
+	fread(&buffer2, sizeof(char), sizeof(uint32_t), fp2);
 	
-	// 16진수의 연산을 위해서 문자열을 16진수 숫자로 변환하기 전에, 0~9 / a~f / A~F만 남깁니다. 
-	clear_str(str1);
-	clear_str(str2);
+	printf("Before ntohl\n%s : %x\n%s : %x\n", argv[1], buffer1, argv[2], buffer2);
 	
-	// 문자열을 16진수 숫자로 바꿔주는 함수 stoh를 이용하여 hex1, hex2에 16진수 숫자를 저장합니다. 
-	uint32_t hex1 = stoh(str1);
-	uint32_t hex2 = stoh(str2);
+	uint32_t n1 = ntohl(buffer1);
+	uint32_t n2 = ntohl(buffer2);
+	uint32_t result = n1 + n2;
+	
+	printf("\nAfter ntohl\n");
+	printf("%d(%x) + %d(%x) = %d(%x)\n", n1, n1, n2, n2, result, result);
 
-	// 과제의 조건인 두 16진수의 합 연산을 실행하여 result 변수에 저장합니다. 
-	uint32_t result = hex1 + hex2;
-	
-	// 결과 값을 10진수와 16진수로 출력합니다. 
-	printf("%d(%#x) + %d(%#x) = %d(%#x)\n", hex1, hex1, hex2, hex2, result, result);
-	
 	fclose(fp2);
 	fclose(fp1);
-	
+
 	return 0;
 }
