b5b1221c470b2618066bc598cb400ce2d9a38725
black6765 <black6765@gmail.com>
2020-08-03 17:31:39 +0900

    Success ARP Request to Gateway

--- src/main.cpp
+++ src/main.cpp
@@ -34,13 +34,13 @@ int main(int argc, char* argv[]) {
 	// find attacker's mac address
 	find_mac(attacker_mac, argv); 
 	// print attacker's mac address
-	printf("%s", attacker_mac);
+	printf("My MAC : %s\n", attacker_mac);
 	find_ip(attacker_ip, argv);
-	printf("%s", attacker_ip);
-	/*
+	printf("My IP : %s\n", attacker_ip);
+	
 	EthArpPacket packet;
 	
-	packet.eth_.dmac_ = Mac(ff:ff:ff:ff:ff:ff);
+	packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
         packet.eth_.smac_ = Mac(attacker_mac);
         packet.eth_.type_ = htons(EthHdr::Arp);
 
@@ -50,11 +50,11 @@ int main(int argc, char* argv[]) {
         packet.arp_.pln_ = Ip::SIZE;
         packet.arp_.op_ = htons(ArpHdr::Request);
         packet.arp_.smac_ = Mac(attacker_mac);
-        packet.arp_.sip_ = htonl(Ip(argv[3]));
-        packet.arp_.tmac_ = Mac(sender_mac);
-        packet.arp_.tip_ = htonl(Ip(argv[2]));
+        packet.arp_.sip_ = htonl(Ip(attacker_ip));
+        packet.arp_.tmac_ = Mac("00:00:00:00:00");
+        packet.arp_.tip_ = htonl(Ip(argv[3]));
 
-	*/
+	
 	/*
 	packet.eth_.dmac_ = Mac(sender_mac);
 	packet.eth_.smac_ = Mac(attacker_mac);
@@ -70,12 +70,11 @@ int main(int argc, char* argv[]) {
 	packet.arp_.tmac_ = Mac(sender_mac);
 	packet.arp_.tip_ = htonl(Ip(argv[2]));
 	*/
-
-	/*	
+		
 	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 	if (res != 0) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
 	}
-	*/
+	
 	pcap_close(handle);
 }
